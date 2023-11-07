a80f8d82a2fc6b416cbc001a1975a690270c1b3a
aqil2514 <muhamadaqil383@gmail.com>
2023-11-07 19:51:09 +0700

    foto

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
   return (
     <nav className="bg-red-400 w-full flex p-3 ps-2 ">
-      <img src="../../public/icon.png" alt="icon" width={"64px"} className="mx-2" />
+      <img src="https://i.imgur.com/72OMYTS.png" alt="icon" width={"64px"} className="mx-2" />
       <h1 className="font-bold text-lime-950 text-2xl antialiased font-serif my-auto">Genshin Random Quotes</h1>
     </nav>
   );
